a35d0e07f2c4254aba031bcb2ff62dfb617cc63c
ammarbagasrawala <ammariddris@gmail.com>
2023-09-24 21:54:22 +0530

    prediction pipeline

--- End_to_End_ML_Project/src/utils.py
+++ End_to_End_ML_Project/src/utils.py
@@ -34,3 +34,10 @@ def evaluate_models(X_train,y_train,X_test,y_test,models):
         return report
     except Exception as e:
         raise CustomException(e,sys)
+
+def load_object(file_path):
+    try:
+        with open(file_path,'rb') as file_obj:
+            return dill.load(file_obj)
+    except Exception as e:
+        raise CustomException(e,sys)
